9c8dbccadad5fcd86812b9057a764dad33d7a32b
WeiYang <weiy6141@gmail.com>
2026-07-27 16:41:37 +0800

    检验单更新质检时间

---
+++ Kingdee.Zitn.Project.Code/Kingdee.Zitn.Project.Code/plugin/QC/QCAuditUpsertDateTime.cs
@@ -0,0 +1,34 @@
+﻿using Kingdee.BOS.Core.DynamicForm.PlugIn;
+using Kingdee.BOS.Core.DynamicForm.PlugIn.Args;
+using Kingdee.BOS.Orm.DataEntity;
+using Kingdee.BOS.Util;
+using System;
+using System.ComponentModel;
+
+namespace Kingdee.Zitn.Project.Code.plugin.QC
+{
+    [Description("【检验单审核服务】:检验单提交，更新之间开始日期为创建日期"), HotUpdate]
+    public class QCAuditUpsertDateTime : AbstractOperationServicePlugIn
+    {
+        public override void AfterExecuteOperationTransaction(AfterExecuteOperationTransaction e)
+        {
+            base.AfterExecuteOperationTransaction(e);
+
+            foreach (DynamicObject obj in e.DataEntitys)
+            {
+                string billno = Convert.ToString(obj["BillNo"]);
+                DateTime createdate = Convert.ToDateTime(obj["CreateDate"]);
+
+                if (string.IsNullOrEmpty(billno) || createdate == DateTime.MinValue)
+                {
+                    return;
+                }
+
+                string sql = string.Format($@"/*dialect*/UPDATE B SET B.FINSPECTSTARTDATE = '{createdate}'
+                FROM T_QM_INSPECTBILL A JOIN T_QM_INSPECTBILLENTRY B ON A.FID = B.FID 
+                WHERE FBILLNO = '{billno}'");
+                Kingdee.BOS.App.Data.DBUtils.Execute(this.Context, sql);
+            }
+        }
+    }
+}
