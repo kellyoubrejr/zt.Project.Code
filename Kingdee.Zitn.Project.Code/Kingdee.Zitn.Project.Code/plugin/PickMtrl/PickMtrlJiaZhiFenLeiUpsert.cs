22f15d16e6339f0da95f58421abbdef26cced43b
WeiYang <weiy6141@gmail.com>
2026-07-28 13:57:39 +0800

    领料单价值分类upsert

--- Kingdee.Zitn.Project.Code/Kingdee.Zitn.Project.Code/plugin/PickMtrl/PickMtrlJiaZhiFenLeiUpsert.cs
+++ Kingdee.Zitn.Project.Code/Kingdee.Zitn.Project.Code/plugin/PickMtrl/PickMtrlJiaZhiFenLeiUpsert.cs
@@ -7,7 +7,7 @@ using Kingdee.BOS.App.Data;
 
 namespace Kingdee.Zitn.Project.Code.plugin.PickMtrl
 {
-    [Description("【服务插件】：生产领料单提交时，根据对应物料的采购价格更新价值分类字段")]
+    [Description("【生产领料服务】：生产领料单提交时，根据对应物料的采购价格更新价值分类字段")]
     [Kingdee.BOS.Util.HotUpdate]
     public class PickMtrlJiaZhiFenLeiUpsert : AbstractOperationServicePlugIn
     {
