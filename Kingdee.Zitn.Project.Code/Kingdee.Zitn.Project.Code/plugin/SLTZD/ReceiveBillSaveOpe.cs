7941585a961bb25c16e387e95791aa3bfcc2cb21
WeiYang <weiy6141@gmail.com>
2026-08-26 13:34:58 +0800

    收料通知单

---
+++ Kingdee.Zitn.Project.Code/Kingdee.Zitn.Project.Code/plugin/SLTZD/ReceiveBillSaveOpe.cs
@@ -0,0 +1,77 @@
+﻿using Kingdee.BOS.App.Data;
+using Kingdee.BOS.Core.DynamicForm.PlugIn;
+using Kingdee.BOS.Core.DynamicForm.PlugIn.Args;
+using Kingdee.BOS.Orm.DataEntity;
+using System;
+using System.ComponentModel;
+
+namespace Kingdee.Zitn.Project.Code.plugin.SLTZD
+{
+    [Description("【收料单保存提交服务】：收料单保存提交时，设置超收信息审批流")]
+    [Kingdee.BOS.Util.HotUpdate]
+    public class ReceiveBillSaveOpe : AbstractOperationServicePlugIn
+    {
+        public override void AfterExecuteOperationTransaction(AfterExecuteOperationTransaction e)
+        {
+            base.AfterExecuteOperationTransaction(e);
+
+            string SLbillno = string.Empty;
+            foreach (DynamicObject obj in e.DataEntitys)
+            {
+                SLbillno = obj["BillNo"].ToString();
+
+                var SLsql = string.Format("/*dialect*/SELECT FACTRECEIVEQTY,FSRCBILLNO,M.FNUMBER,B.FENTRYID FROM T_PUR_Receive A JOIN T_PUR_ReceiveEntry B ON A.FID = B.FID JOIN T_BD_MATERIAL M ON B.FMATERIALID = M.FMATERIALID WHERE FBILLNO = '{0}'", SLbillno);
+                DynamicObjectCollection col = DBUtils.ExecuteDynamicObject(this.Context, SLsql);
+                if (col != null && col.Count > 0)
+                {
+                    for (int i = 0; i < col.Count; i++)
+                    {
+                        int mustqty = Convert.ToInt32(col[i]["FACTRECEIVEQTY"]);
+                        string purno = col[i]["FSRCBILLNO"].ToString();
+                        string wlnum = col[i]["FNUMBER"].ToString();
+                        int entryid = Convert.ToInt32(col[i]["FENTRYID"]);
+
+                        string POsql = $@"SELECT FRECEIVEQTY,FQTY FROM 
+                                    T_PUR_POORDER C JOIN T_PUR_POORDERENTRY D ON C.FID = D.FID 
+                                                                    JOIN T_PUR_POORDERENTRY_r E ON D.FENTRYID=E.FENTRYID
+                                                                    JOIN T_BD_MATERIAL M ON D.FMATERIALID = M.FMATERIALID
+                                                                    WHERE C.FBILLNO = '{purno}' AND M.FNUMBER = '{wlnum}'";
+                        DynamicObjectCollection POcol
+                            = DBUtils.ExecuteDynamicObject(this.Context, POsql);
+                        if (POcol != null && POcol.Count > 0)
+                        {
+                            bool hasFlag = false;
+                            for (int j = 0; j < POcol.Count; j++)
+                            {
+                                int receiveqty = Convert.ToInt32(POcol[j]["FRECEIVEQTY"]);
+                                decimal qty = Convert.ToDecimal(POcol[j]["FQTY"]);
+
+
+                                decimal result = (mustqty + receiveqty - qty) / (decimal)qty;
+                                result = Math.Round(result, 2);
+
+                                if (result > 0)
+                                {
+                                    var upSql = $@"UPDATE T_PUR_RECEIVEENTRY SET FCFBL = {result.ToString("0.00")} WHERE FENTRYID = {entryid} ";
+                                    DBUtils.Execute(this.Context, upSql);
+                                }
+                                if (result > 0.30m)
+                                {
+                                    /*var upSql = $@"UPDATE T_PUR_Receive SET FFLAG = '是' WHERE FBILLNO = {billno} ";
+                                    DBUtils.Execute(this.Context, upSql);*/
+
+                                    hasFlag = true;
+                                }
+                            }
+                            if (hasFlag)
+                            {
+                                string upSql = $@"UPDATE T_PUR_Receive SET FFLAG = '是' WHERE FBILLNO = '{SLbillno}'";
+                                DBUtils.Execute(this.Context, upSql);
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ Kingdee.Zitn.Project.Code/Kingdee.Zitn.Project.Code/plugin/SLTZD/SLTZDautoGetFLot.cs
@@ -0,0 +1,77 @@
+﻿using Kingdee.BOS.Contracts;
+using Kingdee.BOS.Core;
+using Kingdee.BOS.Core.DynamicForm.PlugIn;
+using Kingdee.BOS.Core.Metadata.FieldElement;
+using Kingdee.BOS.Orm.DataEntity;
+using Kingdee.K3.Core.SCM.STK;
+using Kingdee.K3.SCM.Contracts;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+
+namespace Kingdee.Zitn.Project.Code.plugin.SLTZD
+{
+    [Description("【服务插件】收料通知单保存提交，自动获取批号")]
+    [Kingdee.BOS.Util.HotUpdate]
+    public class SLTZDautoGetFLot : AbstractOperationServicePlugIn
+    {
+        public override void BeforeDoSaveExecute(BeforeDoSaveExecuteEventArgs e)
+        {
+            base.BeforeDoSaveExecute(e);
+
+            // 获取对应批号字段
+            LotField lotField = this.BusinessInfo.GetField("Plot") as LotField;
+
+            // 如果批号字段不存在或者没有配置物料和库存组织字段则不处理
+            if (lotField == null || string.IsNullOrWhiteSpace(lotField.OrgFieldKey)
+                || string.IsNullOrWhiteSpace(lotField.ControlFieldKey)
+                || lotField.InputModel != LotField.Enum_InputModel.TextAndSelect)
+            {
+                return;
+            }
+
+            ExtendedDataEntitySet extEntitySet = new ExtendedDataEntitySet();
+            extEntitySet.Parse(e.DataEntities, this.BusinessInfo);
+            ExtendedDataEntity[] entities = extEntitySet.FindByEntityKey(lotField.EntityKey);
+
+            // 调用服务获取主档
+            ILotService lotService = Kingdee.K3.SCM.Contracts.ServiceFactory.GetLotService(this.Context);
+            CodeAppResult codeRet = lotService.GenerateLotMasterByCodeRule(this.Context, this.BusinessInfo, lotField, entities);
+            List<KeyValuePair<int, DynamicObject[]>> lotLists = codeRet.CodeResults;
+
+            if (lotLists == null || lotLists.Count < 1)
+            {
+                return;
+            }
+
+            // 回填单据
+            int index = 0;
+            foreach (ExtendedDataEntity entity in entities)
+            {
+                KeyValuePair<int, DynamicObject[]> lotPair = lotLists.SingleOrDefault(p => p.Key == index);
+                if (lotPair.Value != null && lotPair.Value.Length > 0)
+                {
+                    DynamicObject lot = lotPair.Value[0];
+                    if (lot != null)
+                    {
+                        if (Convert.ToInt64(lot["Id"]) > 0)
+                        {
+                            entity.DataEntity["Lot"] = lot;
+                        }
+                        else
+                        {
+                            entity.DataEntity["Lot_Text"] = lot["Number"];
+                        }
+                    }
+                    else
+                    {
+                        entity.DataEntity["Lot"] = null;
+                        entity.DataEntity["Lot_Text"] = null;
+                    }
+                }
+                index++;
+            }
+        }
+    }
+}
