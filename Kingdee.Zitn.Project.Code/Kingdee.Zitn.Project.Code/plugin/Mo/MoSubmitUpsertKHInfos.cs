f3e53eca5c91b06b83d203ec045fe657e9f62883
WeiYang <weiy6141@gmail.com>
2026-09-20 15:53:31 +0800

    生产订单

---
+++ Kingdee.Zitn.Project.Code/Kingdee.Zitn.Project.Code/plugin/Mo/MoSubmitUpsertKHInfos.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kingdee.Zitn.Project.Code.plugin.Mo
+{
+    internal class MoSubmitUpsertKHInfos
+    {
+    }
+}
